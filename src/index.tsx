a65323b1e3a2b41dd2c478068d60e43d4845c522
Johann Buscail <johann@buscail.eu>
2021-08-31 21:11:38 +0300

    Added props

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,33 @@
 import React from "react";
 import { View, Text } from "react-native";
-import { sum } from "./helpers/sum";
 
 interface Props {
 	/** The name of the app */
 	name: string;
+
+	/** Website url */
+	siteUrl: string;
+
+	/** Api url to save the push notification token */
+	apiUrl: string;
+
+	/** Custom fonts name */
+	fontName?: string;
+
+	/** Custom injected javascript when the site is loading */
+	injectedJavaScript?: string;
 }
 
 /** Main App constructor */
-const App: React.FC<Props> = ({ name }) =>
+const App: React.FC<Props> = ({
+	name, siteUrl, apiUrl, fontName = "custom", injectedJavaScript = "",
+}) =>
 	(
 		<View>
 			<Text>
 				Hello
 				{name}
-				{sum(1, 2)}
+				{fontName}
 			</Text>
 		</View>
 	);
